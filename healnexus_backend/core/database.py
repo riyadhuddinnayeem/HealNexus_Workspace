afe87eb917940066f9ac286208366d8ac56cb71a
riyadhuddinnayeem <riyadhuddinnayeem@gmail.com>
2026-03-29 21:11:47 +0600

    Final working backend: Cloud DB connected with verified dependencies

--- healnexus_backend/core/database.py
+++ healnexus_backend/core/database.py
@@ -1,19 +1,35 @@
+import os
+import certifi
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# MySQL Connection String 
-# Format: mysql+pymysql://<username>:<password>@<host>:<port>/<database_name>
-# If you are using XAMPP, it usually looks exactly like this:
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/healnexus_db"
+# --- TiDB CLOUD SETTINGS ---
+DB_USER = "4HAR75CZ48vTzF5.root"
+DB_PASS = "ket2UJy6Z2YGt6Wu" # <--- PASTE YOUR PASSWORD HERE
+DB_HOST = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com"
+DB_PORT = "4000"
+DB_NAME = "test"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# This line handles the SSL certificate for TiDB Cloud
+ssl_args = {
+    "ssl_verify_cert": True,
+    "ssl_verify_identity": True,
+    "ssl_ca": certifi.where()
+}
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Create engine with SSL arguments
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args=ssl_args
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to get the database session in your routes
 def get_db():
     db = SessionLocal()
     try:
