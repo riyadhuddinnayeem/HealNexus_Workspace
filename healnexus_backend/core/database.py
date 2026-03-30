f19fe23523da301bfddd3ba14af0c0b70fe9d887
riyadhuddinnayeem <riyadhuddinnayeem@gmail.com>
2026-03-30 14:10:17 +0600

    Update database.py
    Add SQLAlchemy pool pinging to fix TiDB connection drops

--- healnexus_backend/core/database.py
+++ healnexus_backend/core/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 1. Fetch the secret URL from the environment (Render's secure vault)
-# If it can't find it, it defaults to your local XAMPP for safe offline testing!
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL", 
     "mysql+pymysql://root:@localhost/healnexus"
@@ -18,9 +17,18 @@ if "tidbcloud" in SQLALCHEMY_DATABASE_URL:
         "ssl_verify_identity": True,
         "ssl_ca": certifi.where()
     }
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=ssl_args)
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, 
+        connect_args=ssl_args,
+        pool_pre_ping=True,   # <--- THE FIX: Ping the DB to ensure it's awake
+        pool_recycle=300      # <--- THE FIX: Refresh connections every 5 minutes
+    )
 else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        pool_pre_ping=True,
+        pool_recycle=300
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
